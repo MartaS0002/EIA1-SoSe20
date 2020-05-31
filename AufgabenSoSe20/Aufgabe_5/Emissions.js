9cf5e2ebe6deb3d64e654f64ec33c51118a69e53
MartaS0002 <64219141+MartaS0002@users.noreply.github.com>
2020-05-31 16:10:28 +0200

    aufgabe 5 uploaded

---
+++ AufgabenSoSe20/Aufgabe_5/Emissions.js
@@ -0,0 +1,76 @@
+"use strict";
+// tslint:disable-next-line: variable-name
+var weltgesamt = 31116.50;
+var europa2018 = 4209.30;
+var europa2 = europa2018 * 100 / weltgesamt;
+var europa2008 = 4965.70;
+var europa3 = europa2018 / europa2008 - 1;
+var europa4 = europa2008 - europa2018;
+console.group("Europa");
+console.log("Die Emission von Europa ist " + europa2018 + "kg CO2.");
+console.log("Relativ zur Gesamtemission der Welt verusacht Europa damit " + europa2 + "%.");
+console.log("Für Europa hat sich 2018 im Vergleich zu 2008 die Emission um " + europa3, "% verändert.");
+console.log("2018 im Vergleich zu 2008 sind das " + europa4, "kg CO2.");
+console.groupEnd();
+// tslint:disable-next-line: variable-name
+var afrika2018 = 1235.50;
+var afrika2008 = 1028;
+var afrika2 = afrika2018 * 100 / weltgesamt;
+var afrika3 = afrika2018 / afrika2008 - 1;
+var afrika4 = afrika2018 - afrika2008;
+console.group("Afrika");
+console.log("Die Emission von Afrika ist " + afrika2018, "kg CO2.");
+console.log("Relativ zur Gesamtemission der Welt verursacht Afrika damit " + afrika2, "%.");
+console.log("Für Afrika hat sich 2018 im Vergleich zu 2008 die Emission um " + afrika3, "% verändert.");
+console.log("2018 im Vergleich zu 2008 sind das " + afrika4, "kg CO2.");
+console.groupEnd();
+console.groupEnd();
+// tslint:disable-next-line: variable-name
+var süda2018 = 1261.50;
+var süda2008 = 1132.60;
+var süda2 = süda2018 * 100 / weltgesamt;
+var süda3 = süda2018 / süda2008 - 1;
+var süda4 = süda2018 - süda2008;
+console.group("Südamerika");
+console.log("Die Emission von Südamerika ist " + süda2018, "kg CO2.");
+console.log("Relativ zur Gesamtemission der welt verursach Südamerika damit " + süda2, "%.");
+console.log("Für Südamerika hat sich 2018 im Vergleich zu 2008 die Emmission um" + süda3, "% verändert.");
+console.log("2018 im Vergleich zu 2008 sind das " + süda4, "kg CO2.");
+console.groupEnd();
+// tslint:disable-next-line: variable-name
+var norda2018 = 6035.60;
+var norda2008 = 6600.40;
+var norda2 = norda2018 * 100 / weltgesamt;
+var norda3 = norda2018 / norda2008 - 1;
+var norda4 = norda2018 - norda2008;
+console.group("Nordamerika");
+console.log("Die Emission von Nordamerika ist " + norda2018, "kg CO2.");
+console.log("Relativ zur Gesamtemission der Welt verursacht Nordamerika damit " + norda2, "%.");
+console.log("Für Nordamerika hat sich 2018 im Vergleich zu 2008 die Emission um" + norda3, "% verändert.");
+console.log("2018 im Vergleich zu 2008 sind das " + norda4, "kg CO2.");
+console.groupEnd();
+// tslint:disable-next-line: variable-name
+var asien2018 = 16274.10;
+var asien2008 = 12954.40;
+var asien2 = asien2018 * 100 / weltgesamt;
+var asien3 = asien2018 / asien2008 - 1;
+var asien4 = asien2018 - asien2008;
+console.group("Asien");
+console.log("Die Emission von Asien ist " + asien2018, "kg CO2.");
+console.log("Relativ zur Gesamtemission der Welt verursacht Asien damit " + asien2 + "%.");
+console.log("Für Asien hat sich 2018 im Vergleich zu 2008 die Emission um " + asien3 + "% verändert.");
+console.log("2018 im Vergleich zu 2008 sind das " + asien4 + "kg CO2.");
+console.groupEnd();
+// tslint:disable-next-line: variable-name
+var aust2018 = 2100.50;
+var aust2008 = 1993.00;
+var aust2 = aust2018 * 100 / weltgesamt;
+var aust3 = aust2018 / aust2008 - 1;
+var aust4 = aust2018 - aust2008;
+console.group("Australien");
+console.log("Die Emission von Australien ist " + aust2018, "kg CO2.");
+console.log("Relativ zur Gesamtemission der Welt verursacht Australien damit " + aust2, "%.");
+console.log("Für Australien hat sich 2018 im Vergleich zu 2008 die Emission um" + aust3, "% verändert.");
+console.log("2018 im Vergleich zu 2008 sind das " + aust4, "kg CO2.");
+console.groupEnd();
+//# sourceMappingURL=Emissions.js.map
---
+++ AufgabenSoSe20/Aufgabe_5/Emissions.ts
@@ -0,0 +1,85 @@
+"use strict";
+// tslint:disable-next-line: variable-name
+var weltgesamt: number = 31116.50;
+var europa2018: number = 4209.30;
+var europa2: number = europa2018 * 100 / weltgesamt;
+var europa2008: number = 4965.70;
+var europa3: number = europa2018 / europa2008 - 1;
+var europa4: number = europa2008 - europa2018;
+console.group("Europa");
+console.log("Die Emission von Europa ist " + europa2018 + "kg CO2.");
+console.log("Relativ zur Gesamtemission der Welt verusacht Europa damit " + europa2 + "%.");
+console.log("Für Europa hat sich 2018 im Vergleich zu 2008 die Emission um " + europa3, "% verändert.");
+console.log("2018 im Vergleich zu 2008 sind das " + europa4, "kg CO2.");
+console.groupEnd();
+
+
+// tslint:disable-next-line: variable-name
+var afrika2018: number = 1235.50;
+var afrika2008: number = 1028;
+var afrika2: number = afrika2018 * 100 / weltgesamt;
+var afrika3: number = afrika2018 / afrika2008 - 1;
+var afrika4: number = afrika2018 - afrika2008;
+console.group("Afrika");
+console.log("Die Emission von Afrika ist " + afrika2018, "kg CO2.");
+console.log("Relativ zur Gesamtemission der Welt verursacht Afrika damit " + afrika2, "%.");
+console.log("Für Afrika hat sich 2018 im Vergleich zu 2008 die Emission um " + afrika3, "% verändert.");
+console.log("2018 im Vergleich zu 2008 sind das " + afrika4, "kg CO2.");
+console.groupEnd();
+console.groupEnd();
+
+
+// tslint:disable-next-line: variable-name
+var süda2018: number = 1261.50;
+var süda2008: number = 1132.60;
+var süda2: number = süda2018 * 100 / weltgesamt;
+var süda3: number = süda2018 / süda2008 - 1;
+var süda4: number = süda2018 - süda2008;
+console.group("Südamerika");
+console.log("Die Emission von Südamerika ist " + süda2018, "kg CO2.");
+console.log("Relativ zur Gesamtemission der welt verursach Südamerika damit " + süda2, "%.");
+console.log("Für Südamerika hat sich 2018 im Vergleich zu 2008 die Emmission um" + süda3, "% verändert.");
+console.log("2018 im Vergleich zu 2008 sind das " + süda4, "kg CO2.");
+console.groupEnd();
+
+
+// tslint:disable-next-line: variable-name
+var norda2018: number = 6035.60;
+var norda2008: number = 6600.40;
+var norda2: number = norda2018 * 100 / weltgesamt;
+var norda3: number = norda2018 / norda2008 - 1;
+var norda4: number = norda2018 - norda2008;
+console.group("Nordamerika");
+console.log("Die Emission von Nordamerika ist " + norda2018, "kg CO2.");
+console.log("Relativ zur Gesamtemission der Welt verursacht Nordamerika damit " + norda2, "%.");
+console.log("Für Nordamerika hat sich 2018 im Vergleich zu 2008 die Emission um" + norda3, "% verändert.");
+console.log("2018 im Vergleich zu 2008 sind das " + norda4, "kg CO2.");
+console.groupEnd();
+
+
+// tslint:disable-next-line: variable-name
+var asien2018: number = 16274.10;
+var asien2008: number = 12954.40;
+var asien2: number = asien2018 * 100 / weltgesamt;
+var asien3: number = asien2018 / asien2008 - 1;
+var asien4: number = asien2018 - asien2008;
+console.group("Asien");
+console.log("Die Emission von Asien ist " + asien2018, "kg CO2.");
+console.log("Relativ zur Gesamtemission der Welt verursacht Asien damit " + asien2 + "%.");
+console.log("Für Asien hat sich 2018 im Vergleich zu 2008 die Emission um " + asien3 + "% verändert.");
+console.log("2018 im Vergleich zu 2008 sind das " + asien4 + "kg CO2.");
+console.groupEnd();
+
+
+// tslint:disable-next-line: variable-name
+var aust2018: number = 2100.50;
+var aust2008: number = 1993.00;
+var aust2: number = aust2018 * 100 / weltgesamt;
+var aust3: number = aust2018 / aust2008 - 1;
+var aust4: number = aust2018 - aust2008;
+console.group("Australien");
+console.log("Die Emission von Australien ist " + aust2018, "kg CO2.");
+console.log("Relativ zur Gesamtemission der Welt verursacht Australien damit " + aust2, "%.");
+console.log("Für Australien hat sich 2018 im Vergleich zu 2008 die Emission um" + aust3, "% verändert.");
+console.log("2018 im Vergleich zu 2008 sind das " + aust4, "kg CO2.");
+console.groupEnd();
